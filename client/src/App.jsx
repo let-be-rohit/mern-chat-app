47fe22fb389f0eaf294d1ae5c9a784f888fd9394
Rohit Kumar <rk2613026@gmail.com>
2024-06-19 16:25:57 +0530

    create signup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 
 import "./App.css";
 import LogIn from './pages/login/LogIn'
+import SignUp from './pages/signup/SignUp'
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <LogIn />
+      <SignUp />
     </div>
   );
 }
---
+++ client/src/pages/signup/SignUp.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import GenderCheckbos from './GenderCheckbos'
+
+const SignUp = () => {
+  return (
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+      <div className='bg-white w-full max-w-md rounded overflow-hidden p-4 mx-auto'>
+      <h1 className='text-3xl font-semibold text-center text-gray-500'>
+        Sign Up
+        <span className='text-fuchsia-500'>  Chat App</span>
+        </h1>
+        <form className='grid gap-4 mt-5'>
+        <div className='flex flex-col gap-1'>
+                <label className='label p-2'>
+                    <span className='text-base label-text'>Full Name</span>
+                </label>
+                <input 
+                type="text" 
+                placeholder='Enter your full name'
+                className='bg-slate-100 px-2 py-1 focus:outline-primary'/>
+            </div>
+            <div className='flex flex-col gap-1'>
+                <label className='label p-2'>
+                    <span className='text-base label-text'>Username</span>
+                </label>
+                <input 
+                type="text" 
+                placeholder='Enter Username'
+                className='bg-slate-100 px-2 py-1 focus:outline-primary'/>
+            </div><div className='flex flex-col gap-1'>
+                <label className='label p-2'>
+                    <span className='text-base label-text'>Password</span>
+                </label>
+                <input 
+                type="text" 
+                placeholder='Enter Password'
+                className='bg-slate-100 px-2 py-1 focus:outline-primary'/>
+            </div><div className='flex flex-col gap-1'>
+                <label className='label p-2'>
+                    <span className='text-base label-text'>Confirm Password</span>
+                </label>
+                <input 
+                type="text" 
+                placeholder='Confirm Password'
+                className='bg-slate-100 px-2 py-1 focus:outline-primary'/>
+            </div>
+
+            <GenderCheckbos />
+
+            <a href="#" className='text-sm hover:underline hover:text-fuchsia-500 mt-2 inline-block'>
+                Already have an account?
+            </a>
+            <div>
+                <button className='btn btn-block btn-sm mt-1 bg-primary'>Sign Up</button>
+            </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default SignUp
