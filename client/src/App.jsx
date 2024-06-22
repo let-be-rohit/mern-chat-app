c437137f88af90dd62f579ac5f818687d247da3e
Rohit Kumar <rk2613026@gmail.com>
2024-06-22 16:00:07 +0530

    completed logout functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,9 +12,9 @@ function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={authUser ? <Home /> : <Navigate to={"/login"}/>} />
         <Route path="/signup" element={authUser ? <Navigate to='/' /> : <SignUp />} />
-        <Route path="/login" element={<LogIn />} />
+        <Route path="/login" element={authUser ? <Navigate to='/' /> : <LogIn />} />
       </Routes>
       <Toaster />
     </div>
